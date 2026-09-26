00e1b2e7e9c41f3e3c44d830e81e261317d0d197
Luke (via Claude) <luke@local>
2026-09-26 09:33:13 -0500

    build a1dda2f

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790430708|3412865';
+const CACHE_VERSION = '1790433192|3439467';
 /** @type {string} */
 const CACHE_PREFIX = 'Luconia-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
